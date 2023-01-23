9c4f9e1f8a92634d8b9a2c597709fe0606e5a3c9
VeeTk <mr.felisity@mail.ru>
2023-01-23 22:26:38 +0300

    Продолжить верстку по макету
    Верстка с JS для навигационной панели

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //=========Выпадашка с номерами================================
 let phoneMenuHeader = function () {
    const arrowActive = document.querySelector(".body__active");
@@ -11,5 +10,21 @@ let phoneMenuHeader = function () {
    });
 };
 phoneMenuHeader();
-// ==============================================================
+// ==================Меню-нав-панель-гл.страницы================
+const menu = document.querySelector(".submenu");
+const link = document.querySelectorAll(".menu-catalog__item");
 
+document.addEventListener("click", documentActions);
+let number = 0;
+
+function documentActions(event) {
+   let numberLink = event.target.getAttribute("data-item");
+
+   if (numberLink == number) {
+      menu.classList.add("_hidden");
+      number = 0;
+   } else {
+      menu.classList.remove("_hidden");
+      number = numberLink;
+   }
+}
